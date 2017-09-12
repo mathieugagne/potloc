35c554c7fa05669a2841a5f3277466032d40d741
Mathieu Gagné <mgagne@vigilantglobal.com>
2017-09-12 12:59:13 -0400

    Validates emails

--- lib/potloc/survey.rb
+++ lib/potloc/survey.rb
@@ -2,15 +2,22 @@
 
 module Potloc
   class Survey
+
+    attr_accessor :emails
+
+    def initialize
+      @emails = []
+    end
+
     def import_csv(file)
       CSV.foreach(file, headers: :first_row) do |row|
         email_question = Question.email
         email = Answer.new(email_question, row).value
-        # TODO: Validate no other answer for that given email has been inserted yet
-        questions.each do |question|
-          # puts question.inspect
-          answer = Answer.new(question, row, email: email)
-          answer.save
+        if valid?(email)
+          questions.each do |question|
+            answer = Answer.new(question, row, email: email)
+            answer.save
+          end
         end
       end
     end
@@ -19,6 +26,11 @@ module Potloc
 
     def questions
       Question.all
+    end
+
+    # Validate that no more than 1 answer per email is allowed
+    def valid?(email)
+      !emails.include?(email) && emails.push(email)
     end
   end
 end
