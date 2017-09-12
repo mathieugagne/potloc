b45af5944c9c44d7276204c91d3909a6eb17a6bc
Mathieu Gagné <mgagne@vigilantglobal.com>
2017-09-12 12:28:45 -0400

    Adds simple Answer model

--- lib/potloc/answer.rb
+++ lib/potloc/answer.rb
@@ -2,5 +2,25 @@
 
 module Potloc
   class Answer
+    attr_reader :email, :question, :row
+
+    def initialize(question, row, email: nil)
+      @email = email
+      @question = question
+      @row = row
+    end
+
+    def save
+      database.exec("INSERT INTO answers (email, question_id, type, value) " \
+              "VALUES ('#{email}', #{question.id}, #{question.category}, #{value}');")
+    end
+
+    def value
+      row.field(question.column)
+    end
+
+    def database
+      Database.new
+    end
   end
 end
