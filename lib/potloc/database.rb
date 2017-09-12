6c176abe58550a2dcfda1818e34968bc3c7e94e7
Mathieu Gagné <mgagne@vigilantglobal.com>
2017-09-12 12:28:59 -0400

    Cleanup Database

--- lib/potloc/database.rb
+++ lib/potloc/database.rb
@@ -3,11 +3,7 @@
 module Potloc
   class Database
     def exec(sql)
-      puts "-----------------------------"
-      puts "-------     SQL  ------------"
-      puts "-----------------------------"
       puts sql
-      puts "********** 15 ms ************"
     end
   end
 end
